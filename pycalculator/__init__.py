be8544042d86b1f5db0fbccd62a989bfa89c1042
juliotrigo <juliotrigocom@gmail.com>
2014-02-19 07:26:37 +0000

    package description.

--- pycalculator/__init__.py
+++ pycalculator/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""pycalculator
+"""pycalculator: a simple calculator in Python.
 
-    A simple calculator in Python.
+    It currently supports the operators: +, -, *, /, **
+    The parser is a deterministic finite automaton (DFA).
+    It reads a string from the command line and returns an element
+    (integer, decimal, operator) each time it is called.
 
     :copyright: (c) 2014 by Julio Vicente Trigo Guijarro.
     :license: BSD, see LICENSE for more details.
